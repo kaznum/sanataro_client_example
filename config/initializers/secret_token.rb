0e0a3cb273618d08c56f1853b4430d2ef9f5ec03
Kazuya NUMATA <numata@gmail.com>
2012-10-19 14:51:52 +0900

    Create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SanataroClientExample::Application.config.secret_token = 'ca4f5fd1c1d2e77adcfe5a40d3af434197a47b484c7c0bdeadb954256cfc2763f705874659ed35fd8be67fef045766fcfbc56ea1710c913802cb1c7be2c04695'
