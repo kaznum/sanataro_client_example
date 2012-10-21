e4352e838216eb097758ec423bdf5d99dfcb1801
Kazuya NUMATA <numata@gmail.com>
2012-10-21 14:13:21 +0900

    Implement Oauth RESTful API access logic with ActiveRecord

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,16 @@
 class EntriesController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :set_oauth
   def index
+    @accounts = Account.all
+    @entries = Entry.all
+  end
+
+  def set_oauth
+    if current_user.sanataro
+      [Entry, Account].each do |c|
+        c.headers['authorization'] = 'Bearer ' + current_user.sanataro.client.token
+      end
+    end
   end
 end
---
+++ app/models/oauth_resource.rb
@@ -0,0 +1,3 @@
+class OauthResource < ActiveResource::Base
+  self.site = "http://localhost:3000/api"
+end
